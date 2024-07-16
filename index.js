eac0b629b9dbe113802012727c150d0bad5052ec
VijaykumarKrish <krishnakamera789@gmail.com>
2024-07-16 16:21:21 +0530

    payload data added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let topic = "e831cd2181f4/command";
 
@@ -80,175 +80,181 @@ let topic = "e831cd2181f4/command";
 
 
 const LaunchRequestHandler = {
-    canHandle(handlerInput) {
-      return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
-    },
-    handle(handlerInput) {
-      const speechText = 'Welcome to smart home. Ask about the window!';
-  
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(speechText)
-        .withSimpleCard('Welcome to smart home. Ask about the window!', speechText)
-        .getResponse();
-    }
-  };
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText = 'Welcome to smart home. Ask about the window!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('Welcome to smart home. Ask about the window!', speechText)
+      .getResponse();
+  }
+};
 
 
-  
 
-  const AskWindowIntentHandler = {
-    canHandle(handlerInput) {
-      return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-        && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AskWindowIntents';
-    },
-    handle(handlerInput) {
-      console.log("handlerInput: ",handlerInput);
-      let speechText = 'window is started to working';
 
-      const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
-      const slots = handlerInput.requestEnvelope.request.intent.slots;
-      const userQuery = handlerInput.requestEnvelope.request.intent.slots.window.value;
-      const question = handlerInput.requestEnvelope.request.intent.slots.query.value;
+const AskWindowIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AskWindowIntents';
+  },
+  handle(handlerInput) {
+    console.log("handlerInput: ", handlerInput);
+    let speechText = 'window is started to working';
 
-      if(question.startsWith("move")){
+    const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+    const slots = handlerInput.requestEnvelope.request.intent.slots;
+    const userQuery = handlerInput.requestEnvelope.request.intent.slots.window.value;
+    const question = handlerInput.requestEnvelope.request.intent.slots.query.value;
 
-        //let payload = [0x0d ,0x0f, 0x00, 0x30, 0x05, 0x96, 0x73, 0x00, 0x20, 0x01, 0x64, 0x07, 0x64]
-        const extractedNumbers = extractNumbersFromSentence(question);
-       if(extractedNumbers.length === 0){
+    if (question.startsWith("move")) {
+
+      //let payload = [0x0d ,0x0f, 0x00, 0x30, 0x05, 0x96, 0x73, 0x00, 0x20, 0x01, 0x64, 0x07, 0x64]
+      let extractedNumbers = extractNumbersFromSentence(question);
+      if (extractedNumbers.length === 0) {
         speechText = "please provide any prcentage"
-       }else{
-        let percentageVlaue = (parseInt(extractedNumbers[0])*1000)/100;
+      } else {
+        let percentageVlaue = (parseInt(extractedNumbers[0]) * 1000) / 100;
         const littleEndianBuffer = decimalToLittleEndian(percentageVlaue);
         console.log(littleEndianBuffer);
         let payload = `000f cf00 0073 9605 2001 ${littleEndianBuffer} 0732`;
+        const formattedArray = payload.split(' ')
+          .flatMap(hexPair => hexPair.match(/.{1,2}/g) || [])
+          .map(hex => `0x${hex}`);
 
-        // client.publish('e831cd2181f4/command', Buffer.from(payload, 'hex'));
-        console.log("payload "+Buffer.from(payload, 'hex'));
+        // client.publish('e831cd2181f4/command', formattedArray.join(" "));
+
+        console.log("move: ",formattedArray.join(" "));
+
         speechText = "window is ready to move";
-       }
-      
-      }else if(question.startsWith("open")){
 
-        let arr = `000f cf00 0073 9605 2001 e803 0732`;
-
-        
-
-      //  client.publish('e831cd2181f4/command', Buffer.from(arr, 'hex'));
-
-        speechText = "window is ready to open";
-      }else if(question.startsWith("close")){
-        let payload ='000f cf00 0073 9605 2001 0000 0732';
-        // client.publish('e831cd2181f4/command', Buffer.from(arr, 'hex'));
-
-        speechText = "window is ready to close";
-      }else{
-        speechText = "please tell me the proper command";
       }
-     
-  
-      console.log(`Intent received: ${intentName}`);
-      console.log(`User query: ${userQuery}`);
-      console.log(`Slots: ${JSON.stringify(slots)}`);
-      console.log(`question: ${JSON.stringify(question)}`);
-  
 
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        // .withShouldEndSession(false)
-        // .reprompt("ok")
-        .withSimpleCard(speechText)
-        .getResponse();
+    } else if (question.startsWith("open")) {
+
+      let data = `0x00 0x0f 0xcf 0x00 0x00 0x73 0x96 0x05 0x20 0x01 0xe8 0x03 0x07 0x32`;
+      //  client.publish('e831cd2181f4/command', data);
+      console.log("open: ",data);
+
+      speechText = "window is ready to open";
+    } else if (question.startsWith("close")) {
+
+      let data = '0x00 0x0f 0xcf 0x00 0x00 0x73 0x96 0x05 0x20 0x01 0x00 0x00 0x07 0x32';
+      // client.publish('e831cd2181f4/command', data);
+      console.log("close: ", data);
+
+      speechText = "window is ready to close";
+    } else {
+      speechText = "please tell me the proper command";
     }
-  };
+
+
+    console.log(`Intent received: ${intentName}`);
+    console.log(`User query: ${userQuery}`);
+    console.log(`Slots: ${JSON.stringify(slots)}`);
+    console.log(`question: ${JSON.stringify(question)}`);
+
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      // .withShouldEndSession(false)
+      // .reprompt("ok")
+      .withSimpleCard(speechText)
+      .getResponse();
+  }
+};
 
 
 
 
-  const AskWeatherIntentHandler = {
-    canHandle(handlerInput) {
-      return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-        && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AskWeatherIntents';
-    },
-    handle(handlerInput) {
-      console.log("handlerInput: ",handlerInput);
-      const speechText = 'The weather today is sunny.';
+const AskWeatherIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AskWeatherIntents';
+  },
+  handle(handlerInput) {
+    console.log("handlerInput: ", handlerInput);
+    const speechText = 'The weather today is sunny.';
 
-      const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
-      const slots = handlerInput.requestEnvelope.request.intent.slots;
-      const userQuery = handlerInput.requestEnvelope.request.intent.slots.InfoType.value;
-      const fullQuestion = handlerInput.requestEnvelope.request.intent.slots.FullQuestion.value;
+    const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+    const slots = handlerInput.requestEnvelope.request.intent.slots;
+    const userQuery = handlerInput.requestEnvelope.request.intent.slots.InfoType.value;
+    const fullQuestion = handlerInput.requestEnvelope.request.intent.slots.FullQuestion.value;
 
 
-      console.log(`Full question: ${fullQuestion}`);
-     
-  
-      console.log(`Intent received: ${intentName}`);
-      console.log(`User query: ${userQuery}`);
-      console.log(`Slots: ${JSON.stringify(slots)}`);
-  
+    console.log(`Full question: ${fullQuestion}`);
 
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .withShouldEndSession(false)
-        .withSimpleCard('The weather today is sunny.', speechText)
-        .getResponse();
-    }
-  };
-  const HelpIntentHandler = {
-    canHandle(handlerInput) {
-      return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-        && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
-    },
-    handle(handlerInput) {
-      const speechText = 'You can ask me the weather!';
-  
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(speechText)
-        .withSimpleCard('You can ask me the weather!', speechText)
-        .getResponse();
-    }
-  };
-  const CancelAndStopIntentHandler = {
-    canHandle(handlerInput) {
-      return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-        && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
-          || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
-    },
-    handle(handlerInput) {
-      const speechText = 'Goodbye!';
-  
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .withSimpleCard('Goodbye!', speechText)
-        .withShouldEndSession(true)
-        .getResponse();
-    }
-  };
-  const SessionEndedRequestHandler = {
-    canHandle(handlerInput) {
-      return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
-    },
-    handle(handlerInput) {
-      // Any clean-up logic goes here.
-      return handlerInput.responseBuilder.getResponse();
-    }
-  };
 
-  const ErrorHandler = {
-    canHandle() {
-      return true;
-    },
-    handle(handlerInput, error) {
-      console.log(`Error handled: ${error.message}`);
-  
-      return handlerInput.responseBuilder
-        .speak('Sorry, I don\'t understand your command. Please say it again.')
-        .reprompt('Sorry, I don\'t understand your command. Please say it again.')
-        .getResponse();
-    }
-  };
+    console.log(`Intent received: ${intentName}`);
+    console.log(`User query: ${userQuery}`);
+    console.log(`Slots: ${JSON.stringify(slots)}`);
+
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withShouldEndSession(false)
+      .withSimpleCard('The weather today is sunny.', speechText)
+      .getResponse();
+  }
+};
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'You can ask me the weather!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('You can ask me the weather!', speechText)
+      .getResponse();
+  }
+};
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+        || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Goodbye!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('Goodbye!', speechText)
+      .withShouldEndSession(true)
+      .getResponse();
+  }
+};
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    // Any clean-up logic goes here.
+    return handlerInput.responseBuilder.getResponse();
+  }
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I don\'t understand your command. Please say it again.')
+      .reprompt('Sorry, I don\'t understand your command. Please say it again.')
+      .getResponse();
+  }
+};
 
 //   let skill;
 
@@ -281,7 +287,7 @@ skill = Alexa.SkillBuilders.custom()
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
-    )
+  )
   .addErrorHandlers(ErrorHandler)
   .create();
 
@@ -305,21 +311,21 @@ skill = Alexa.SkillBuilders.custom()
 // const skill = skillBuilder.create();
 const adapter = new ExpressAdapter(skill, false, false);
 
-app.get('/get', (req,res) => {
-    res.send("getting working!!!")
+app.get('/get', (req, res) => {
+  res.send("getting working!!!")
 })
 
 
 app.post('/alexa', async (req, res) => {
-        console.log('Received request:', JSON.stringify(req.body, null, 2));
-        try {
-            const response = await skill.invoke(req.body);
-            res.json(response);
-        } catch (error) {
-            console.error('Error handling the request:', error);
-            res.status(500).send('Error handling the request');
-        }
-    });
+  console.log('Received request:', JSON.stringify(req.body, null, 2));
+  try {
+    const response = await skill.invoke(req.body);
+    res.json(response);
+  } catch (error) {
+    console.error('Error handling the request:', error);
+    res.status(500).send('Error handling the request');
+  }
+});
 
 app.post('/', adapter.getRequestHandlers());
 
@@ -332,13 +338,13 @@ function connect() {
 
   // let decodedString = Buffer.from(encodedString, 'base64').toString("binary");
 
-  
+
   // let decodedString = atob(encodedString);
 
-  this.mqttClient = mqtt.connect("mqtts://ec2-3-226-44-11.compute-1.amazonaws.com:8883", { username: "celllabs", password: "celllabs",  ca: encodedString },
-  );
-  // this.mqttClient = mqtt.connect("mqtt://krishna@localhost:1883");
-  
+  // this.mqttClient = mqtt.connect("mqtts://ec2-3-226-44-11.compute-1.amazonaws.com:8883", { username: "celllabs", password: "celllabs",  ca: encodedString },
+  // );
+  this.mqttClient = mqtt.connect("mqtt://krishna@localhost:1883");
+
 
   // Mqtt error calback
   this.mqttClient.on('error', (err) => {
@@ -352,7 +358,7 @@ function connect() {
   });
 
   // mqtt subscriptions
-  this.mqttClient.subscribe('e831cd2181f4/command', {qos: 0});
+  this.mqttClient.subscribe('e831cd2181f4/command', { qos: 0 });
 
   // When a message arrives, console.log it
   this.mqttClient.on('message', function (topic, message) {
@@ -362,7 +368,7 @@ function connect() {
   this.mqttClient.on('close', () => {
     console.log(`mqtt client disconnected`);
   });
-  
+
   return this.mqttClient;
 }
 
@@ -375,8 +381,12 @@ function extractNumbersFromSentence(sentence) {
   const numbers = sentence.match(/\d+/g);
   return numbers ? numbers : [];
 }
+function insertSpacesInHex(hexString) {
+  const result = hexString.match(/.{1,2}/g).join(' ');
+  return result;
+}
 
- // Output: <Buffer 96 75 16 0>
+// Output: <Buffer 96 75 16 0>
 
 
 
